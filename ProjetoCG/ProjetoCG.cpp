1146e821d78374e3b59ea34c665fabfaf13699dc
Polpedu <emailparaoedu@gmail.com>
2023-01-12 03:21:20 +0000

    new meta updates

--- ProjetoCG/ProjetoCG.cpp
+++ ProjetoCG/ProjetoCG.cpp
@@ -30,32 +30,20 @@
 #include <stdio.h>
 #include <math.h>
 #include <GL/glut.h>
+#include "RgbImage.h"
+
 #define PI		 3.14159
 
 //--------------------------------- Definir cores
-#define BLUE     0.0, 0.0, 1.0, 1.0
-#define YELLOW	 1.0, 1.0, 0.0, 1.0
-#define GREEN    0.0, 1.0, 0.0, 1.0
-#define ORANGE   1.0, 0.5, 0.1, 1.0
-#define CYAN     0.0, 1.0, 1.0, 1.0
 #define WHITE    1.0, 1.0, 1.0, 1.0
-#define BLACK    0.0, 0.0, 0.0, 1.0
-#define GRAY     0.55, 0.55, 0.55, 1.0
-#define RED		 0.651, 0.1725, 0.1686, 1.0
-#define RED_H	 1.0, 0.0, 0.0, 1.0
-#define PURPLE	 0.5, 0.0, 0.5, 1.0
-
-#define C_GRAS   0.4862745098, 0.98823529411, 0.0
 
 #define INITIAL_BACKLEN -50.0
 #define INITIAL_FRONTLEN 50.0
 #define GROUND_LEVEL	-11.0
 
-// An array with all the colors
-GLfloat colorsrandom[10][4] = { {BLUE}, {YELLOW}, {GREEN}, {ORANGE}, {CYAN}, {WHITE}, {BLACK}, {GRAY}, {RED}, {PURPLE} };
 
-//function that returns a random color
-GLfloat* randomColor() { return colorsrandom[rand() % 10]; }
+void initMaterials(int material);
+
 
 //================================================================================
 //------------------------------------------------------------ Sistema Coordenadas 
@@ -67,7 +55,6 @@ GLfloat		SKY_HEIGHT = 50;
 GLfloat	  self_x = 0.0, self_y = 0.0, self_z = 0.0;
 GLfloat	  rotation = 0;
 GLfloat	  rotor_angle = 0.0;
-// GLfloat	  door_angle = 0.0;
 GLfloat	  heli_tilt = 0.0;
 GLfloat	  animacaoJanela = 0, invert = -1;
 GLfloat   altura = 0;
@@ -75,31 +62,11 @@ GLfloat   incALT = 0.1;
 
 GLfloat   heliRadius = 5.5;
 
+GLUquadricObj* esfera = gluNewQuadric();
 
-//coordinates of cube for sky
-GLfloat vertices_sky[8][3] = {
-	{-SKY_SIZE, -SKY_HEIGHT, -SKY_SIZE},
-	{ SKY_SIZE, -SKY_HEIGHT, -SKY_SIZE},
-	{ SKY_SIZE, SKY_SIZE, -SKY_SIZE},
-	{-SKY_SIZE, SKY_SIZE, -SKY_SIZE},
-	{-SKY_SIZE, -SKY_HEIGHT,  SKY_SIZE},
-	{ SKY_SIZE, -SKY_HEIGHT,  SKY_SIZE},
-	{ SKY_SIZE, SKY_SIZE,  SKY_SIZE},
-	{-SKY_SIZE, SKY_SIZE,  SKY_SIZE}
-};
-
-//colors of skycube
-GLdouble colors[8][4] = {
-	{0.8862745098039216,1.0,1.0,0.5},
-	{0.8862745098039216,1.0,1.0,0.5},
-	{0.4,0.8784313725490196,1.0,0.5},
-	{0.4,0.8784313725490196,1.0,0.5},
-	{0.8862745098039216,1.0,1.0,0.5},
-	{0.8862745098039216,1.0,1.0,0.5},
-	{0.4,0.8784313725490196,1.0,0.5},
-	{0.4,0.8784313725490196,1.0,0.5}
-};
-
+GLint   Dia = 1;     //:::   'D' - Day
+GLfloat intensidadeDia = 1.0;
+GLfloat luzGlobalCorAmb[4] = { intensidadeDia, intensidadeDia,intensidadeDia, 1.0 }; // cor ambiente da luz global
 
 //============================================================= Observador
 GLfloat  rVisao =	8, aVisao = -0.5 * PI, incVisao = 1;
@@ -108,16 +75,103 @@ GLfloat	 at[]	=	{ 0.0,  0.0,  0.0 };
 GLfloat	 anguloZ= 35;
 
 
+GLfloat   luzR = 1.0;		 	 //:::   'R'  
+GLfloat   luzG = 1.0;			 //:::   'G'  
+GLfloat   luzB = 1.0;			 //:::   'B'  
+GLfloat localPos[4] = { 0.0, 5.0, 0.0, 1.0 };
+GLfloat localCorAmb[4] = { 1, 1, 1, 0.0 };
+GLfloat localCorDif[4] = { luzR, luzG, luzB, 1.0 };
+GLfloat localCorEsp[4] = { luzR, luzG, luzB, 1.0 };
+
+char     texto[30];
+
 //=========================================================== Variaveis e constantes
 GLint     msec = 50;	//.. tempo entre frames - milisegundos
-GLint	  GRASS_VOLUME = 2500;
-
+GLfloat  xC = 2500.0;
 //=========================================================== Flags
 int is_forward = 0, is_left = 0, is_right = 0, is_back = 0;
 
+//=========================================================== Textures
+GLuint   texture[5];
+RgbImage imag;
+static GLfloat texturas[] = {
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1,
+0, 0,
+1, 0,
+1, 1,
+0, 1 };
+
+GLdouble normalGround[] = { 0.0, 1.0, 0.0 };
+
 
 //========================================================= Objects
-void d_polygon(int a, int b, int c, int d);
 void d_helicopter();
 void d_body();
 void d_rotor();
@@ -133,17 +187,13 @@ void translate_window();
 // void d_mountain();
 void d_sky();
 // void d_clouds();
-// void d_road();
 void fly_effect();
-
-// void d_door(); not sure if this is going to be implemented
 
 //========================================================= Helicopter
 void d_helicopter()
 {
+	
 	glPushMatrix();
-		
-	glColor4f(RED);
 	
 		glTranslatef(self_x, self_y, self_z);
 		fly_effect();
@@ -153,7 +203,10 @@ void d_helicopter()
 		glTranslatef(self_x, self_y, self_z);
 		glRotatef(heli_tilt, 0.0, 0.0, 1.0);  //tilting
 
+		initMaterials(11);
+		glPushMatrix();
 		d_body();
+		glPopMatrix();
 
 		d_rotor();
 
@@ -171,7 +224,7 @@ void d_helicopter()
 }
 
 void d_window() {
-	glColor4f(PURPLE);//grey
+	initMaterials(2);
 	//point upwards
 	glRotated(-90.0, 1.0, 0.0, 0.0);
 
@@ -212,6 +265,7 @@ void translate_window() {
 
 // rotor rotation logic
 void rotation_rotor() {
+	// first animaton rotor_angle
 	if (rotor_angle < 100) rotor_angle += 200.0;
 	else if (rotor_angle < 200) rotor_angle += 20.5;
 	else if (rotor_angle < 300) rotor_angle += 300.0;
@@ -251,8 +305,8 @@ void d_legs()
 {
 	glPushMatrix();
 
-	glColor4f(0.7, 0.7, 0.7, 1.0);//grey
-
+	initMaterials(9);
+	
 	glBegin(GL_LINES);
 	//first leg
 	glVertex3f(5, -7, 7);
@@ -295,8 +349,8 @@ void d_tail()
 {
 	//tail of heli
 	glPushMatrix();
-	glColor4f(GRAY);//grey
-
+	initMaterials(1);
+	
 	glBegin(GL_LINE_LOOP);
 	glVertex3f(1.2, -2.0, -9.0);
 	glVertex3f(1.2, 2.0, -9.0);
@@ -330,6 +384,7 @@ void d_tail()
 }
 
 void d_body() {
+	
 	glPushMatrix();
 	glScalef(1.0, 1.0, 2.0);
 	glutSolidSphere(heliRadius, 32, 32);
@@ -340,7 +395,7 @@ void d_body() {
 void d_rotor() {
 	glPushMatrix();
 
-	glColor4f(GRAY);//silver
+	initMaterials(1);
 	glLineWidth(5.0);
 
 	glBegin(GL_LINES);//fan stick
@@ -380,12 +435,17 @@ void d_rotor() {
 //======================================================
 
 void d_pad() {
-	glColor4f(YELLOW);
+	initMaterials(16);
+
 	glPushMatrix();
 	glBegin(GL_QUADS);
+	glNormal3dv(normalGround);
 	glVertex3f(-10.0, -10.0, -10.0);
+	glNormal3dv(normalGround);
 	glVertex3f(10.0, -10.0, -10.0);
+	glNormal3dv(normalGround);
 	glVertex3f(10.0, -10.0, 10.0);
+	glNormal3dv(normalGround);
 	glVertex3f(-10.0, -10.0, 10.0);
 	glEnd();
 	glPopMatrix();
@@ -396,12 +456,12 @@ void d_house()
 
 	glPushMatrix();
 
-	glColor4f(RED_H);
+	initMaterials(15);
 	glTranslatef(0.0, 5.0, 0.0);
 	glutSolidCube(25.0);
 	glTranslatef(0.0, -5.0, 0.0);
 
-	glColor4f(BLUE);
+	initMaterials(1);
 	glTranslatef(0.0, 15.0, 0.0);
 	glRotatef(-90.0, 1.0, 0.0, 0.0);
 	gluCylinder(gluNewQuadric(), 25.0, 0.0, 25.0, 32, 32);
@@ -442,61 +502,117 @@ void d_buildings() {
 	glPopMatrix();
 }
 
-// Function to create a polygon
-void d_polygon(int a, int b, int c, int d) {
-	glPushMatrix();
-	
-		glBegin(GL_POLYGON);
-		glColor4dv(colors[a]);
-		glVertex3fv(vertices_sky[a]);
-		glColor4dv(colors[b]);
-		glVertex3fv(vertices_sky[b]);
-		glColor4dv(colors[c]);
-		glVertex3fv(vertices_sky[c]);
-		glColor4dv(colors[d]);
-		glVertex3fv(vertices_sky[d]);
-		glEnd();
-		
-	glPopMatrix();
-}
-
 // Sky is intended to move with the "camera"
 void d_sky() {
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, texture[!Dia]);
 	glPushMatrix();
-	//regra da mão direita
-	d_polygon(0, 3, 2, 1);
-	d_polygon(0,4,7,3);
-	d_polygon(2,3,7,6);
-	d_polygon(1,2,6,5);
-	d_polygon(4,5,6,7);
+	glRotatef(-90, 1, 0, 0);
+	gluQuadricDrawStyle(esfera, GLU_FILL);
+	gluQuadricNormals(esfera, GLU_SMOOTH);
+	gluQuadricTexture(esfera, GL_TRUE);
+	gluSphere(esfera, 800.0, 100, 100);
 	glPopMatrix();
+	glDisable(GL_TEXTURE_2D);
 }
 
 void d_grass() {
+	float L = 0.8;
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, texture[2]);
+	initMaterials(0);
 	glPushMatrix();
-	glColor3f(C_GRAS);
-	
 	// simple plane has grass
-	glBegin(GL_QUADS);
-	glVertex3f(GRASS_VOLUME, GROUND_LEVEL, GRASS_VOLUME);
-	glVertex3f(-GRASS_VOLUME, GROUND_LEVEL, GRASS_VOLUME);
-	glVertex3f(-GRASS_VOLUME, GROUND_LEVEL, -GRASS_VOLUME);
-	glVertex3f(GRASS_VOLUME, GROUND_LEVEL, -GRASS_VOLUME);
-	glEnd();
-		
+		glBegin(GL_QUADS);
+		glNormal3dv(normalGround);			glTexCoord3f(0.0f, 0.0f, 0.0f);		glVertex3i(-L * xC, -11, L * xC); //A 
+		glNormal3dv(normalGround);			glTexCoord3f(6.0f, 0.0f, 0.0f);		glVertex3i(L * xC, -11, L * xC);  //B
+		glNormal3dv(normalGround);			glTexCoord3f(6.0f, 6.0f, 0.0f);		glVertex3i(L * xC, -11, -L * xC); //C
+		glNormal3dv(normalGround);			glTexCoord3f(0.0f, 6.0f, 0.0f);		glVertex3i(-L * xC, -11, -L * xC); //D
+		glEnd();
 	glPopMatrix();
+	glDisable(GL_TEXTURE_2D);
+}
+
+void desenhaTexto(char* string, GLfloat x, GLfloat y, GLfloat z) {
+	glRasterPos3f(x, y, z);
+	while (*string)
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_10, *string++);
 }
  
+void initLights(void) {
+	//����������������������������������������������������� Ambiente
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, luzGlobalCorAmb);
+
+	//����������������������������������������������������� Teto
+	/*glLightfv(GL_LIGHT0, GL_POSITION, localPos);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, localCorAmb);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, localCorDif);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, localCorEsp);*/
+}
+
+void initTexturas()
+{
+	//----------------------------------------- Chao - tapete
+	glGenTextures(1, &texture[0]);
+	glBindTexture(GL_TEXTURE_2D, texture[0]);
+	imag.LoadBmpFile("sky.bmp");
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3,
+		imag.GetNumCols(),
+		imag.GetNumRows(), 0, GL_RGB, GL_UNSIGNED_BYTE,
+		imag.ImageData());
+
+	glGenTextures(1, &texture[1]);
+	glBindTexture(GL_TEXTURE_2D, texture[1]);
+	imag.LoadBmpFile("noite.bmp");
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3,
+		imag.GetNumCols(),
+		imag.GetNumRows(), 0, GL_RGB, GL_UNSIGNED_BYTE,
+		imag.ImageData());
+
+	glGenTextures(1, &texture[2]);
+	glBindTexture(GL_TEXTURE_2D, texture[2]);
+	imag.LoadBmpFile("grass.bmp");
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, Dia ? GL_MODULATE : GL_DECAL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3,
+		imag.GetNumCols(),
+		imag.GetNumRows(), 0, GL_RGB, GL_UNSIGNED_BYTE,
+		imag.ImageData());
+
+}
+
+
 //=========================================================================== Init
 void initialize(void)
 {
-	glEnable(GL_DEPTH_TEST);	//������������������������������Profundidade
+	glClearColor(WHITE);
+	glShadeModel(GL_SMOOTH);
+	initTexturas();
 
-	// PARA SE VER TUDO, dou disable a esconder overall:
-	// glDisable(GL_CULL_FACE);		//������������������������������Faces visiveis
+	glEnable(GL_DEPTH_TEST);	//������������������������������Profundidade
+	glEnable(GL_LIGHTING);
+
 	
-	
-	glEnableClientState(GL_COLOR_ARRAY);
+	glNormalPointer(GL_FLOAT, 0, normalGround);
+	glEnableClientState(GL_NORMAL_ARRAY);
+	glTexCoordPointer(2, GL_FLOAT, 0, texturas);   // coordenadas textura
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+	initLights();
+	initMaterials(11);
 }
 
 //======================================
@@ -509,10 +625,12 @@ void drawScene() {
 	glTranslatef(obsP[0], obsP[1], obsP[2]);
 	d_sky();
 	glTranslatef(-obsP[0], -obsP[1], -obsP[2]);
+
+	sprintf_s(texto, 30, "%d - Dia 'D' ", Dia);
+	desenhaTexto(texto, -12, 1, -14);
 	
 	/*printf("%f\n", obsP[1]);
 	printf("%f\n", self_y);*/
-
 }
 
 //====================================================== 
@@ -529,9 +647,6 @@ void display(void) {
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();  // Reset the model-view matrix
 
-	//======================================================
-	//  <><><><><><><>         OBSERVADOR NAO EST� FIXO ????
-	//======================================================
 	gluLookAt(obsP[0], obsP[1], obsP[2], at[0], at[1], at[2], 0, 1, 0); // girar a camera em si, as coordenadas são dadas por obsP e o ponto para apontar é 0 0 0
 
 	//=================================================================
@@ -592,6 +707,22 @@ void keyboard(unsigned char key, int x, int y) {
 			obsP[1] += ammount;
 			at[1] += ammount;
 			break;
+
+		case 'd':case 'D':
+			Dia = !Dia;
+			if (Dia) {
+				luzGlobalCorAmb[0] = 1.0;
+				luzGlobalCorAmb[1] = 1.0;
+				luzGlobalCorAmb[2] = 1.0;
+			}
+			else {
+				luzGlobalCorAmb[0] = 0.20;
+				luzGlobalCorAmb[1] = 0.20;
+				luzGlobalCorAmb[2] = 0.20;
+			}
+			glLightModelfv(GL_LIGHT_MODEL_AMBIENT, luzGlobalCorAmb);
+			glutPostRedisplay();
+			break;
 		case 27:
 			exit(0);
 			break;
@@ -643,9 +774,7 @@ void releasekeyboard(int key, int x, int y)
 	if (key == GLUT_KEY_RIGHT)	{ is_right = 0; }
 	if (key == GLUT_KEY_LEFT)	{ is_left = 0; }
 	if (key == GLUT_KEY_DOWN)	{ is_back = 0; }
-
 }
-
 
 //======================================================= MAIN
 //======================================================= MAIN
@@ -664,9 +793,6 @@ int main(int argc, char** argv) {
 	glutSpecialFunc(teclasNotAscii);
 	glutSpecialUpFunc(releasekeyboard);
 
-	
-	
-	
 	glutTimerFunc(msec, Timer, 1);
 
 	glutMainLoop();
